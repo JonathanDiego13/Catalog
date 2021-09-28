97a8c4d05f9063920a106d2a9cb9de888947a129
Jonathan Diego <jonathan.mendoza@luuna.mx>
2021-09-28 18:26:38 -0500

    Create a permission for products and users api, create a many to many model, create logic for track of the number of times every single productis queried and the last details of produts api

---
+++ apps/products/models/product_user_relationship.py
@@ -0,0 +1,32 @@
+"""ProductUserRelationship model."""
+
+# Django
+from django.db import models
+
+# Utilities
+from apps.utils.models import CatalogModel
+
+
+class ProductUserRelationship(CatalogModel):
+    """ProductUserRelationship model.
+    """
+
+    user = models.ForeignKey(
+        'users.User',
+        on_delete=models.CASCADE,
+    )
+
+    product = models.ForeignKey(
+        'products.Product',
+        on_delete=models.CASCADE,
+    )
+
+    # Stats
+    times_queried_number = models.PositiveBigIntegerField(default=0)
+
+    def __str__(self):
+        """Return username and circle."""
+        return '@{} at #{}'.format(
+            self.user.username,
+            self.product.sku
+        )
